54703eb74a306f664fa813fc4adaa867935b7246
Aditi Chakravarthi <aditi@one45.com>
2016-04-23 13:50:08 -0700

    working copy with create_stream function

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,10 +1,13 @@
 from lxml import html
 import requests
+from populate_test_data import populate_test_data
+
 
 page = requests.get('http://waterdata.usgs.gov/mi/nwis/uv?cb_00055=on&cb_00010=on&format=html&site_no=04119400&period=&begin_date=2016-01-01&end_date=2016-04-23')
 tree = html.fromstring(page.content)
 
-
+stream = tree.xpath('/html/body/h2/text()')
+print stream
 
 date = tree.xpath('/html/body/table[1]/tbody//tr/td[1]/text()')
 print "Dates Jan - April", date
@@ -14,3 +17,8 @@ print "Velocity Jan - April", velocity
 
 temp = tree.xpath('/html/body/table[1]/tbody/tr/td[3]/text()')
 print "Temperature Jan - April", temp
+
+def stream_create(stream_name):
+    populate_test_data.create_stream(stream_name)
+
+stream_create("hello")
