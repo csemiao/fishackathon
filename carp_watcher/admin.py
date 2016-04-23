0767568acc7449a46fac476f629a40ca0a8d5d0a
Chris Semiao <csemiao@gmail.com>
2016-04-23 10:32:58 -0700

    models - initial implementation

--- carp_watcher/admin.py
+++ carp_watcher/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from carp_watcher.models import Fish, Stream, Data_Stream, Data_Stream_Temp
 
 # Register your models here.
+
+admin.site.register(Fish)
+admin.site.register(Stream)
+admin.site.register(Data_Stream)
+admin.site.register(Data_Stream_Temp)
--- carp_watcher/models.py
+++ carp_watcher/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+Fish model
+"""
+class Fish (models.Model):
+    name = models.CharField(max_length=50)
+    coefficient = models.DecimalField(max_digits=10, decimal_places=2)
+    exponent = models.DecimalField(max_digits=10, decimal_places=8)
+
+    def __unicode__(self):
+        return self.name
+
+"""
+Stream model
+"""
+class Stream (models.Model):
+    name = models.CharField(max_length=50)
+    coeff_x2 = models.DecimalField(max_digits=7, decimal_places=4, default=0)
+    coeff_x = models.DecimalField(max_digits=7, decimal_places=4, default=0)
+    coeff_con = models.DecimalField(max_digits=7, decimal_places=4, default=0)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __unicode__(self):
+        return self.name
+
+"""
+Daily stream data for all streams
+"""
+class Data_Stream(models.Model):
+    stream = models.ForeignKey(Stream)
+    velocity = models.DecimalField(max_digits=7, decimal_places=4)
+    discharge = models.DecimalField(max_digits=7, decimal_places=2)
+    day = models.DateTimeField()
+
+    class Meta:
+        ordering = ('stream', 'day')
+
+    def __unicode__(self):
+        return self.stream + " " + self.day
+
+"""
+Daily stream temperature for all streams
+"""
+class Data_Stream_Temp(models.Model):
+    stream = models.ForeignKey(Stream)
+    temp = models.IntegerField()
+    day = models.DateTimeField()
+
+    class Meta:
+        ordering = ('stream', 'day')
+
+    def __unicode__(self):
+        return self.steam + " " + self.day
