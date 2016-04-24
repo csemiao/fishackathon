2af3e2e44485f0f8182b1db23e297cac5d970177
Alek Hrycaiko <alekhrycaiko@gmail.com>
2016-04-23 18:01:34 -0700

    mock data implementation and basic styling for maps.

--- carp_watcher/static/js/maps.js
+++ carp_watcher/static/js/maps.js
@@ -3,38 +3,41 @@ Author: alek
 Purpose: Init map alongside points with back-end.
 GET gets list.
 */
+
 var map;
-/*var blueMarker = new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/blue-dot.png")
-var yellowMarker = new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/yellow-dot.png")
-var redMarker = new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/red-dot.png")
-var orangeMarker = new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/orange-dot.png")
-var greenMarker = new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/green-dot.png")
-*/
 
 var data = [ {
   name: 'Point Grey River',
   lat: '49.1320129',
   lng: '-123.1',
+  suitable: 'major'
 },
 {
 name: 'A Grey River',
 lat: '50.11',
 lng: '-119.57',
+suitable: 'minor'
 },
 {
 name: 'Some Grey River',
 lat: '54.2',
 lng: '-115.73',
+suitable: 'invalid'
+
 },
 {
 name: 'lol a Grey River',
 lat: '52.71',
 lng: '-113.43',
+suitable: 'major'
+
 },
 {
 name: 'i wont swim and hate Grey Rivers',
 lat: '49.1320129',
 lng: '-123.1',
+suitable: 'minor'
+
 },
 ]
 
@@ -47,37 +50,64 @@ function setupMap() {
     center: americaCenter,
     zoom: 3
   });
-
 }
 
-function processData(info){
-  var marker, markerColor, blueMarker, yellowMarker, redMArker, orangeMarker, greenMarker, position;
+function setInfoWindowMarkup () {
+  var html;
+  html = '<div class = "cw-flow-status"> Hi this is my flow status'
+        +'<div class="cw-graph">graph goes here. scale size as necessary</div>'
+        + '</div>'
+  return html;
+}
 
-  if (info){ // if we have info to parse
+
+function setUpInfoWindow (info, i, marker) {
+  var infowindow = new google.maps.InfoWindow({
+    content: setInfoWindowMarkup()
+  });
+  marker.addListener('click',function(){
+    infowindow.open(map, marker);
+  });
+};
+
+function processData(info){
+  var marker, markerColor, position, greenMarker, icon;
+  greenMarker =  new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/green-dot.png")
+
+  if (info){
   for (var i =0; i < info.length; i++){
-    // lat lng,
     position = {lat: parseInt(info[i].lat), lng: parseInt(info[i].lng)};
     marker = new google.maps.Marker({
-    icon:  markerColor,
+    icon: greenMarker,
     map: map,
     position: position,
     title: info[i].name});
-    var infowindow = new google.maps.InfoWindow({
-      content: info[i].name
-    });
-    marker.addListener('click',function(){
-      infowindow.open(map, marker);
-    })
+    // setupInfoWindow will need to be passed the suitable status in text.
+    setUpInfoWindow(info, i, marker);
     }
   }
 }
+function handleGetMarkerDataSuccess (response) {
+  alert('success');
+}
+function handleGetMarkerDataError(error){
+  alert('error!');
+}
+
+function getMarkerData(){
+  $.ajax({
+    url: "/showall/",
+    type: "get", //send it through get method
+    success: handleGetMarkerDataSuccess(response),
+    error: handleGetMarkerDataError(error)
+  });
+}
 
 function initMap() {
+  var info;
   setupMap();
-  var info = data;
+  // GET request.
+//  info = getMarkerData();
+  info = data;
   processData(info);
-    // init Map needs to make a GET request.
-    // init Map needs to setup the map.
-    // init Map then needs to process the information.
-    // init Map then needs to plot the points.
 };
