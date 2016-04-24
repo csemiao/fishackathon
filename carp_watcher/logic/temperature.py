24779e82c2f14899edcdd4db915c9117eca02df2
helenly <helen.choi@alumni.ubc.ca>
2016-04-24 01:47:57 -0700

    add temp fn

--- carp_watcher/logic/temperature.py
+++ carp_watcher/logic/temperature.py
@@ -25,3 +25,16 @@ def get_temp_graph_data(stream):
         to_return.append(dict)
 
     return to_return
+
+def get_water_temp(stream):
+    date = datetime.datetime.now()
+    current_year = datetime.datetime.strftime(date, "%Y")
+    current_month = datetime.datetime.strftime(date, "%m")
+    current_day = datetime.datetime.strftime(date, "%d")
+
+    the_streams = Data_Stream.objects.filter(stream = stream).filter(day__year=current_year, day__month=current_month,
+                                                                    day__day=current_day)
+    if len(the_streams) == 1:
+        return the_streams[0].temp
+    else:
+        return -1
