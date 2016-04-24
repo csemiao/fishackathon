32ce04547748e0de6c8e6b35a48e65bad016e69b
helenly <helen.choi@alumni.ubc.ca>
2016-04-23 19:31:05 -0700

    implement logic; temp and velocity returns -1 if no data is available for that stream on that day

--- carp_watcher/logic/temperature.py
+++ carp_watcher/logic/temperature.py
@@ -1,5 +1,19 @@
+import datetime
+
+from carp_watcher.models import Data_Stream
+
 __author__ = 'Chris'
 
 
 def get_water_temp(stream):
-    return 22
+    date = datetime.datetime.now()
+    current_year = datetime.datetime.strftime(date, "%Y")
+    current_month = datetime.datetime.strftime(date, "%m")
+    current_day = datetime.datetime.strftime(date, "%d")
+
+    the_streams = Data_Stream.objects.filter(stream = stream).filter(day__year=current_year, day__month=current_month,
+                                                                    day__day=current_day)
+    if len(the_streams) == 1:
+        return the_streams[0].temp
+    else:
+        return -1
